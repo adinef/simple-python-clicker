520c7605dc73fd4dd99e269727e38bc6f6301aec
Fijałkowski <fian@gft.com>
2021-05-14 11:32:06 +0200

    Add hotkey to config.ini

--- run_from_config.bat
+++ run_from_config.bat
@@ -5,11 +5,13 @@ if %%a==x set x=%%b
 if %%a==y set y=%%b
 if %%a==delay set delay=%%b
 if %%a==window set window=%%b
+if %%a==hotkey set hotkey=%%b
 )
 
 echo X: %x%
 echo Y: %y%
 echo Delay: %delay%
 echo Window: %window%
+echo Hotkey: %hotkey%
 
-python C:\Users\fian\move_me\mc.py --x %x% --y %y% --delay %delay% --window %window%
+python C:\Users\fian\move_me\mc.py --x %x% --y %y% --delay %delay% --window %window% --exit_key %hotkey%
