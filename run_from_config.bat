fbeca9caf6f9b9cad5710dd41132b47c6cae3260
Fijałkowski <fian@gft.com>
2021-05-14 11:25:44 +0200

    Remove run_on_idle; Add run from config for windows execution using config.ini

---
+++ run_from_config.bat
@@ -0,0 +1,16 @@
+@echo off
+
+@echo off
+for /f "tokens=1,2 delims==" %%a in (config.ini) do (
+if %%a==x set x=%%b
+if %%a==y set y=%%b
+if %%a==delay set delay=%%b
+if %%a==window set window=%%b
+)
+
+echo X: %x%
+echo Y: %y%
+echo Delay: %delay%
+echo Window: %window%
+
+python C:\Users\fian\move_me\mc.py --x %x% --y %y% --delay %delay% --window %window%
